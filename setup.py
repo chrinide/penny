785fd3b167f35f0d810cc0401541eea28c825a89
Jonathon Morgan <jonathon@goodattheinternet.com>
2014-10-02 10:58:08 -0500

    adding address module to setup, incrementing version

--- setup.py
+++ setup.py
@@ -8,17 +8,17 @@ except (IOError, ImportError):
    long_description = open('README.md').read()
 
 setup(name='penny',
-      version='0.3.2',
+      version='0.3.3',
       description='Inspect csv files and lists of data',
       long_description=long_description,
       url='https://github.com/gati/penny',
-      download_url ='https://github.com/gati/penny/tarball/0.3.2',
+      download_url ='https://github.com/gati/penny/tarball/0.3.3',
       author='Jonathon Morgan',
       author_email='jonathon@goodattheinternet.com',
       license='MIT',
       packages=['penny'],
       test_suite='tests',
-      install_requires=['python-dateutil'],
+      install_requires=['python-dateutil','address'],
       package_data = {
             'penny': ['data/*.csv'],
       },
