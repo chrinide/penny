dbcc8a0e1f39c0cfe14ee1a3a42c8ca19589dec2
Jonathon Morgan <jonathon@goodattheinternet.com>
2014-12-04 13:51:30 -0600

    incrementing version

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ except (IOError, ImportError):
    long_description = open('README.md').read()
 
 setup(name='penny',
-      version='0.3.10',
+      version='0.3.11',
       description='Inspect your data. Find the truth!',
       long_description=long_description,
       url='https://github.com/gati/penny',
-      download_url ='https://github.com/gati/penny/tarball/0.3.10',
+      download_url ='https://github.com/gati/penny/tarball/0.3.11',
       author='Jonathon Morgan',
       author_email='jonathon@goodattheinternet.com',
       license='MIT',
